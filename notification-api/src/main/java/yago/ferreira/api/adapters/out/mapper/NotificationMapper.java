35db4a5fae262b1d7a1577de86ed7ca1264ce5c8
yagof <yagofer12@gmail.com>
2025-08-04 21:40:38 -0300

    Dtos

---
+++ notification-api/src/main/java/yago/ferreira/api/adapters/out/mapper/NotificationMapper.java
@@ -0,0 +1,11 @@
+package yago.ferreira.api.adapters.out.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface NotificationMapper {
+    NotificationMapper INSTANCE = Mappers.getMapper(NotificationMapper.class);
+
+    
+}
